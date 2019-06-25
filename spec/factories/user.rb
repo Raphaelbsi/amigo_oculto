ac025bfcff9b3112d37116b0a1ab9af36f8232c5
Raphael Souza <oliveira_raphael@msn.com>
2019-06-25 14:10:23 -0300

    Adjust test campaign

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
     factory :user do
       name         { FFaker::Lorem.word }
       email        { FFaker::Internet.email }
-      password     'secret123'
+      password     {'secret123'}
+
     end
    end
